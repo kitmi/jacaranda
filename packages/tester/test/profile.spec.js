dbc0e16fe88038398c609c62a6c04cf5ffae05b6
Rockie Guo <rockie@kitmi.com.au>
2023-12-11 19:49:37 +0800

    Update profile.spec.js

--- packages/tester/test/profile.spec.js
+++ packages/tester/test/profile.spec.js
@@ -1,7 +1,7 @@
 import { cmd } from '@kitmi/sys';
 import path from 'node:path';
 
-describe.only('test-profiling', function () {
+describe('test-profiling', function () {
     it(`benchmarkCase1`, async function () {
         await jacat.profile_('benchmarkCase1', () => {
             const file = path.relative(process.cwd(), path.resolve(__dirname, './benchmarkCase1.js'));
