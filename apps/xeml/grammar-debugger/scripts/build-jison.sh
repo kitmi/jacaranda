fd0200cb1541f82fa0441b270859e97bfbb41474
Rockie Guo <rockie@kitmi.com.au>
2024-05-29 13:46:02 +0800

    chore: remove grammar-debugger

--- apps/xeml/grammar-debugger/scripts/build-jison.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# build up the version of jison.js used in the web worker
-cd node_modules/jison
-npm install
-cd -
-./node_modules/.bin/browserify --exports require node_modules/jison/entry.js > src/util/jison.js
--- apps/xeml/scripts/build-grammar.sh
+++ apps/xeml/scripts/build-grammar.sh
@@ -2,6 +2,6 @@
 
 ./node_modules/.bin/jison ./src/lang/grammar/xeml.jison -m commonjs -o ./src/lang/grammar/xeml.js 
 
-cp ./src/lang/grammar/xeml.jison ./grammar-debugger/public/xeml.jison
+# cp ./src/lang/grammar/xeml.jison ./grammar-debugger/public/xeml.jison
 
 npm run build
